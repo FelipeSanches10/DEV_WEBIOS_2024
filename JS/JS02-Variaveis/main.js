b4020061aba2152dc69a232b76121537e9ecfe75
FelipeSanches10 <fvieirasanches@gmail.com>
2024-05-08 21:44:09 -0300

    Aula js

---
+++ JS/JS02-Variaveis/main.js
@@ -0,0 +1,49 @@
+// var _felipe = 8;
+
+// console.log(_felipe);
+
+// var _felipe = 10;
+// _felipe  = 12;
+// console.log(_felipe);
+
+// const repeted = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve("Loading for send in API...");
+//     }, 2000);
+// })
+
+// .then((response) => {
+//     console.log(response);
+// })
+
+// .catch((error) => {
+//     console.log(error);
+// });
+
+
+
+// const values = {
+//     address: "São Paulo",
+//     number: 154,
+//     street: "Rua Alameda"
+// };
+
+// console.log(values);
+
+// const nan = 0 / 0;
+
+// console.log(nan);
+
+// let IOS = "Instituto da Oportunidade Social";
+
+// console.log(`O nome do curso é ${IOS}`);
+
+
+var number = 2;
+
+console.log(`Eu tenho ${number} reais.`);
+
+let num1 = 4;
+let num2 = 4;
+
+console.log(num1 == num2);
