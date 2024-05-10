dbcc3d2b49c67303db3d16e8b0c2836562b87906
FelipeSanches10 <fvieirasanches@gmail.com>
2024-05-09 21:42:38 -0300

    JS

--- JS/JS02-Variaveis/main.js
+++ JS/JS02-Variaveis/main.js
@@ -39,11 +39,45 @@
 // console.log(`O nome do curso é ${IOS}`);
 
 
-var number = 2;
+// var number = 2;
 
-console.log(`Eu tenho ${number} reais.`);
+// console.log(`Eu tenho ${number} reais.`);
 
-let num1 = 4;
-let num2 = 4;
+// let num1 = 4;
+// let num2 = 4;
 
-console.log(num1 == num2);
+// console.log(num1 == num2);
+
+
+//Operadores
+
+// const operator1 = 8;
+// const operator2 = null;
+
+// console.log(operator1 * operator2);
+
+// const operator3 = "5";
+// const operator4 = 1;
+
+// console.log(operator3 - operator4);
+
+// const operator5 = "5";
+// const operator6 = 1;
+
+// console.log(operator5 + operator6);
+
+// const operator7 = "five";
+// const operator8 = 1;
+
+// console.log(operator7 * operator8);
+
+
+// const value1 = 2;
+// const value2 = 2;
+// const res = value1 % value2;
+
+// console.log(res);
+
+const value = "2" === 2;
+console.log(value);
+
