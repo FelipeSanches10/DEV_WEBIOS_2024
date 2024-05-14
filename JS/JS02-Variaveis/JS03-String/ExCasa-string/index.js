1d049a11f71f6ada7e99d817b07682fb8473c3c4
FelipeSanches10 <fvieirasanches@gmail.com>
2024-05-13 21:45:23 -0300

    strings

---
+++ JS/JS02-Variaveis/JS03-String/ExCasa-string/index.js
@@ -0,0 +1,9 @@
+const text = "Estamos aprendendo JavaScript e seremos futuros Devs";
+
+console.log(text.charAt(19));
+
+console.log(text.length);
+
+console.log(text.toLocaleUpperCase());
+
+console.log(text.substring(48));
---
+++ JS/JS02-Variaveis/JS03-String/index.js
@@ -0,0 +1,16 @@
+// const text = 'ios fez uma excursão na TOTVS com tres cabritinhos(alunos)';
+
+// console.log(text.charAt(2));
+
+// let text2 = "ios";
+// let text3 = "Está chovendo hamburguer";
+
+// console.log(`Na variavel text2 tem ${text2.length} caracteres, e na variavel text3 tem ${text3.length} caracteres`);
+
+var str = 'Mozilla';
+
+console.log(str);
+console.log(str.substring(1, 3));
+console.log(str.substring(3)); 
+
+
