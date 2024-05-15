c9f184afe4b45dd16154e4161adc2263df8bccc6
FelipeSanches10 <fvieirasanches@gmail.com>
2024-05-14 21:39:09 -0300

    Condicional

---
+++ JS/JS02-Variaveis/JS04-Condicionais/index.js
@@ -0,0 +1,34 @@
+// const todosDeCracha = true;
+
+
+// if (todosDeCracha == true ) {
+//     console.log("Todos receberam nota 10 em js");
+// } else if ( todosDeCracha != true) {
+//     console.log("Todos receberam nota 0 em js");
+// }
+
+
+// const num1 = 4;
+// const num2 = 2; 
+
+// if ( num1 > num2 ) {
+//     console.log('Variavel 1 é maior que variavel 2');
+// } else if (num1 < num2) {
+//     console.log('Variavel 1 é menor que variavel 2');
+// }
+
+// num1 > num2 ? console.log(`O valor da variavel num1 é ${num1}, e é maior que o valor da variavel num2 que é ${num2}`) : console.log(`O valor ${num1} é menor que o valor ${num2}`);
+
+// let mes = "Janeiro";
+
+// if ( mes == "Janeiro") {
+//     console.log("É verão");
+// } 
+// else if (mes == "Junho") {
+//     console.log("É outono");
+// } 
+// else {
+//     console.log("É primavera");
+// }
+
+
