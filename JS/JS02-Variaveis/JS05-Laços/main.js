566b8347033fc45e205d395c6f21d5a6f3c29b86
FelipeSanches10 <fvieirasanches@gmail.com>
2024-05-15 21:42:04 -0300

    Laços

---
+++ JS/JS02-Variaveis/JS05-Laços/main.js
@@ -0,0 +1,19 @@
+// let content = 0;
+
+// while ( content <= 10 ) {
+//     console.log(`0 ainda  é ${content}`);
+//     content++;
+// } 
+
+// var counter = 0
+// var result = true;
+// do {
+//     console.log(`${counter} é menor que 10 ou seja é ${result}`);
+//     counter++;
+// } while(counter < 10);
+
+
+const value = 10
+for(let i = 0; i < 10; i++) {
+    console.log(i);
+}
