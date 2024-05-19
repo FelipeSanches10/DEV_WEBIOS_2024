64ec5aabf5a696470448ac9adb5c4eb9f4b938d1
FelipeSanches10 <fvieirasanches@gmail.com>
2024-05-19 11:47:11 -0300

    tabuada

---
+++ JS/JS02-Variaveis/JS05-Laços/ExCasa/main.js
@@ -0,0 +1,16 @@
+let tab2 = 2;
+let tab3 = 3;
+
+
+
+if ( tab2 == 2 ) {
+    for(let i = 0; i <= 10; i++) {
+        console.log(`2 x ${i} = ${i * 2}`);
+    } 
+    
+} 
+if ( tab3 == 3 ) {
+    for(let i = 0; i <= 10; i++) {
+        console.log(`3 x ${i} = ${i * 3}`);
+    }
+}
