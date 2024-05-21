4c3e9d5fa6f5177cdd2a5472d877502cc56e94c2
FelipeSanches10 <fvieirasanches@gmail.com>
2024-05-21 19:56:02 -0300

    Objetos array

---
+++ JS/JS02-Variaveis/JS07-ArrayObjeto/main.js
@@ -0,0 +1,12 @@
+const person = {
+    firstName: 'Felipe',
+    lastName: 'Vieira',
+    age: 19,
+    address: {
+        street: 'Rua teste',
+        state: 'São Paulo',
+        number: '175'
+    },
+};
+
+console.log(person);
