42c3a17a6d8c2e170ac47ae37aaeb985c2034da1
FelipeSanches10 <fvieirasanches@gmail.com>
2024-05-23 21:39:57 -0300

    Function

---
+++ JS/JS02-Variaveis/JS08-Function/main.js
@@ -0,0 +1,28 @@
+// function sum(a, b, c) {
+//     return a + b + c
+// }
+
+// console.log(sum(10, 20, 30));
+
+//Function
+// function values(n1, n2, n3, n4) {
+//     return (n1 + n2 + n3 + n4) / 4
+// }
+
+// console.log(values(7, 8, 6, 5));
+
+
+
+// //Arrow Function
+// const values2 = ((n1, n2, n3, n4) => {
+//     return (n1 + n2 + n3 + n4) / 4;
+// });
+
+// console.log(values2(5, 7, 6, 9));
+
+
+
+const clickon = () => {
+    alert("Sua tela está carregada bem vindo");
+    console.log("Recarregou a tela");
+}
